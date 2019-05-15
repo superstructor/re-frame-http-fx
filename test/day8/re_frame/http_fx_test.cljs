4cd1cd9421977cf144f05835a69109ce1421ae39
Isaac Johnston <isaac@superstruct.nz>
2019-05-15 20:49:59 +0800

    Upgrade ClojureScript to 1.10.520
    Closes #34

--- test/day8/re_frame/http_fx_test.cljs
+++ test/day8/re_frame/http_fx_test.cljs
@@ -1,8 +1,8 @@
 (ns day8.re-frame.http-fx-test
   (:require
-    [ajax.core     :as ajax]
-    [cljs.test     :refer-macros [is deftest async use-fixtures]]
-    [cljs.spec     :as s]
+    [ajax.core :as ajax]
+    [clojure.test :refer [is deftest async use-fixtures]]
+    [clojure.spec.alpha :as s]
     [re-frame.core :as re-frame]
     [day8.re-frame.http-fx]))
 
